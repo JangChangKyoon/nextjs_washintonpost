94389a3e7407d9e9b95b6ce84bb8dc88faa8c490
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-17 23:51:27 +0900

    Api fetch

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function Potato() {
   return (
     <div>
       <Seo title="About" />
-      <h1>About</h1>
+      <h1>개발이 정발 어렵다는 것을 알게된 8개월차의 어느 봄날</h1>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import Seo from "./components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {results?.map((post) => (
+        <div key={post.list_name}>
+          <h4>{post.display_name}</h4>
+        </div>
+      ))}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/posts`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
